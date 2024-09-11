e7d6c104c160a43f7f87704e6029abdfe8788f9d
d33p0st <d33p0st@gmail.com>
2024-09-11 05:54:41 +0530

    added sync codes

---
+++ codes/sync.py
@@ -0,0 +1,39 @@
+import os
+from git import Repo
+
+# class lister:
+#     def __init__(self, directory: str = os.getcwd()):
+#         self.dir = directory
+    
+#     @property
+#     def scan(self) -> list[str]:
+#         entries = []
+
+#         for dirpath, dirnames, filenames in os.walk(self.dir):
+#             for dirname in dirnames:
+#                 entries.append(os.path.join(dirpath, dirname))
+#             for filename in filenames:
+#                 entries.append(os.path.join(dirpath, filename))
+        
+#         return entries
+
+#     def list(self, file: str = os.path.join(os.getcwd(), '__all_files__')):
+#         with open(file, 'w+') as f_r:
+#             for f in self.scan:
+#                 f_r.write(f + "\n")
+
+class sync:
+    def __init__(self):
+        token = os.environ['GITHUB_TOKEN']
+        repo = Repo(os.getcwd())
+
+        if repo.is_dirty(untracked_files=True):
+            repo.index.add('*')
+            repo.index.commit(f'mix-bin added by bot.')
+            origin = repo.remote()
+            origin.push()
+            print("Updates pushed!")
+        else:
+            print("No change detected.")
+
+sync()
